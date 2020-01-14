336acf794a6edb3ce1195905454c995e0e3f1bee
rakesh-mth <46665723+rakesh-mth@users.noreply.github.com>
2020-01-14 16:29:32 -0500

    add commands for creating symlinks on linux

---
+++ LINUX/create-symlink.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -snf ~/workspace/rakesh-mth/dotfiles/GIT/.gitconfig ~/.gitconfig
+ln -snf ~/workspace/scripts/GIT/.gitconfig ~/.gitconfig-work
+
+# symlink related to editors
+ln -snf ~/workspace/rakesh-mth/dotfiles/EDITORS/VIM/init.vim ~/.vimrc
+ln -snf ~/workspace/rakesh-mth/dotfiles/EDITORS/VIM/init.vim ~/.config/nvim/init.vim
