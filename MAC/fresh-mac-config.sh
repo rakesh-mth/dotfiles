4a13b77834accbb76c87b857e44517da4104d644
rakesh-mth <46665723+rakesh-mth@users.noreply.github.com>
2020-01-26 13:45:21 -0500

    add python development modueles. seperate outa python command in fresh-mac-config.

--- MAC/fresh-mac-config.sh
+++ MAC/fresh-mac-config.sh
@@ -1,5 +1,5 @@
-brew install neovim vim fd ripgrep the_silver_searcher fzf ctags wget xz jfrog-cli-go python@2 python@3 nodejs golang rust llvm cmake aspell 2>&1 | tee ~/brew/fresh-mac-config
-brew cask install emacs 2>&1 | tee ~/brew/emacs
+brew install neovim vim emacs fd ripgrep the_silver_searcher fzf ctags wget xz jfrog-cli-go python@2 python@3 nodejs golang rust llvm cmake aspell 2>&1 | tee ~/brew/fresh-mac-config
+# brew cask install emacs 2>&1 | tee ~/brew/emacs
 
 
 pip2 install --upgrade pip
@@ -9,7 +9,24 @@ pip3 install --user pynvim neovim-remote virtualenvwrapper # add ~/Library/Pytho
 
 npm install -g neovim tern typescript yarn
 gem install --user-install neovim # add ~/.gem/ruby/2.6.0/bin in your path (change version number)
-mkdir -p ~/software # create software folder for fonts and other opensource
+
+# create software folder for fonts and other opensource
+mkdir -p ~/software 
+
+
+################################################################################
+######PYTHON
+# install python packages in virtualenv - required for python development
+virtualenv -p /usr/local/bin/python2.7 $HOME/virtualenvs/python27
+virtualenv -p /usr/local/bin/python3.7 $HOME/virtualenvs/python37
+# work related development
+source $HOME/virtualenvs/python27/bin/activate
+pip install pynvim setuptools dagger enum pyOpenSSL PyYAML requests decorator networkx==1.10 
+# python development in emacs or vim
+source $HOME/virtualenvs/python37/bin/activate
+pip install pynvim neovim-remote flake8 isort yapf python-language-server pyls-isort pyls-mypy importmagic epc ptvsd autoflake 
+################################################################################
+
 
 ################################################################################
 ######EMACS
@@ -17,10 +34,6 @@ mkdir -p ~/software # create software folder for fonts and other opensource
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 # font for emacs
 git clone https://github.com/adobe-fonts/source-code-pro.git ~/software/source-code-pro
-# install python packages in virtualenv - required for python development
-virtualenv -p /usr/local/bin/python3.7 $HOME/virtualenvs/python37
-source $HOME/virtualenvs/python37/bin/activate
-pip install flake8 isort yapf python-language-server pyls-isort pyls-mypy importmagic epc ptvsd autoflake
 ################################################################################
 
 
@@ -29,3 +42,5 @@ pip install flake8 isort yapf python-language-server pyls-isort pyls-mypy import
 # font for vim
 git clone https://github.com/powerline/fonts.git ~/software/fonts
 ################################################################################
+
+
