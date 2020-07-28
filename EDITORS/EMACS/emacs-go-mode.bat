ba3e117b9264f5fcf59e014828366a3e6a6f53c5
Rakesh Kumar <rakesh.kumar2@citrix.com>
2020-07-28 15:03:05 -0400

    update emacs go command

--- EDITORS/EMACS/emacs-go-mode.bat
+++ EDITORS/EMACS/emacs-go-mode.bat
@@ -1,5 +1,11 @@
-set GO111MODULE=on
+set GO111MODULE=on 
 go get -v golang.org/x/tools/gopls@latest
+set CGO_ENABLED=0
+REM ldflag option does not work in windows
+REM go get -v -trimpath -ldflags '-s -w' github.com/golangci/golangci-lint/cmd/golangci-lint
+go get -v -trimpath github.com/golangci/golangci-lint/cmd/golangci-lint
+set GO111MODULE=
+set CGO_ENABLED=
 go get -u -v golang.org/x/tools/cmd/godoc
 go get -u -v golang.org/x/tools/cmd/goimports
 go get -u -v golang.org/x/tools/cmd/gorename
@@ -8,7 +14,6 @@ go get -u -v github.com/cweill/gotests/...
 go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
 go get -u -v github.com/fatih/gomodifytags
 go get -u -v github.com/godoctor/godoctor
-go get -u -v github.com/golangci/golangci-lint/cmd/golangci-lint
 go get -u -v github.com/haya14busa/gopkgs/cmd/gopkgs
 go get -u -v github.com/josharian/impl
 go get -u -v github.com/mdempsky/gocode
