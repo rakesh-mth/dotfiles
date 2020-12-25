0e8746d44fb77ff8aa4bc41b6ae7051eacf14746
rakesh-mth <46665723+rakesh-mth@users.noreply.github.com>
2020-12-25 17:47:58 -0500

    add .emacs and .emacs-profile.el files

---
+++ EDITORS/EMACS/.emacs-profiles.el
@@ -0,0 +1,7 @@
+(
+    ("default" . ((user-emacs-directory . "~/emacs/.emacs.d")))
+    ("spacemacs" . ((user-emacs-directory . "~/emacs/spacemacs/.emacs.d")
+                    (env . (("SPACEMACSDIR" . "~/emacs/spacemacs/.spacemacs.d")))))
+    ("doom" . ((user-emacs-directory . "~/emacs/doom-emacs/.emacs.d")
+               (env . (("DOOMDIR" . "~/emacs/doom-emacs/.doom.d")))))
+)
