0e88088527564e34fede359dffca87ef5623e39d
rakesh-mth <46665723+rakesh-mth@users.noreply.github.com>
2020-12-27 22:24:43 -0500

    use ~/.config/emacs on windows

--- WINDOWS/create-symlink.bat
+++ WINDOWS/create-symlink.bat
@@ -3,15 +3,15 @@ if exist %USERPROFILE%\.gitconfig-work del %USERPROFILE%\.gitconfig-work
 if exist %USERPROFILE%\.wslconfig del %USERPROFILE%\.wslconfig
 if exist %USERPROFILE%\.emacs del %USERPROFILE%\.emacs
 if exist %USERPROFILE%\.emacs-profiles.el del %USERPROFILE%\.emacs-profiles.el
-if exist %USERPROFILE%\emacs\spacemacs\.spacemacs.d rmdir %USERPROFILE%\emacs\spacemacs\.spacemacs.d
-if exist %USERPROFILE%\emacs\doom-emacs\.doom.d rmdir %USERPROFILE%\emacs\doom-emacs\.doom.d
+if exist %USERPROFILE%\.config\emacs\spacemacs\.spacemacs.d rmdir %USERPROFILE%\.config\emacs\spacemacs\.spacemacs.d
+if exist %USERPROFILE%\.config\emacs\doom-emacs\.doom.d rmdir %USERPROFILE%\.config\emacs\doom-emacs\.doom.d
 if exist %USERPROFILE%\.vimrc del %USERPROFILE%\.vimrc
 if exist %USERPROFILE%\AppData\Local\nvim\init.vim del %USERPROFILE%\AppData\Local\nvim\init.vim
 
 if not exist %USERPROFILE%\AppData\Local\nvim mkdir %USERPROFILE%\AppData\Local\nvim
 if not exist %USERPROFILE%\.vim mkdir %USERPROFILE%\.vim
-if not exist %USERPROFILE%\emacs\spacemacs mkdir %USERPROFILE%\emacs\spacemacs
-if not exist %USERPROFILE%\emacs\doom-emacs mkdir %USERPROFILE%\emacs\doom-emacs
+if not exist %USERPROFILE%\.config\emacs\spacemacs mkdir %USERPROFILE%\.config\emacs\spacemacs
+if not exist %USERPROFILE%\.config\emacs\doom-emacs mkdir %USERPROFILE%\.config\emacs\doom-emacs
 
 SET SCRIPTS=f:\DevTrees\scripts
 SET DOTFILES=f:\DevTrees\rakesh-mth\dotfiles
@@ -24,9 +24,9 @@ mklink %USERPROFILE%\AppData\Local\nvim\init.vim %DOTFILES%\EDITORS\VIM\init.vim
 REM chemacs - spacemacs and doom-emacs
 mklink %USERPROFILE%\.emacs %DOTFILES%\EDITORS\EMACS\.emacs
 mklink %USERPROFILE%\.emacs-profiles.el %DOTFILES%\EDITORS\EMACS\.emacs-profiles.el
-mklink /D %USERPROFILE%\emacs\spacemacs\.spacemacs.d %DOTFILES%\EDITORS\EMACS\.spacemacs.d
-mklink /D %USERPROFILE%\emacs\doom-emacs\.doom.d %DOTFILES%\EDITORS\EMACS\.doom.d
+mklink /D %USERPROFILE%\.config\emacs\spacemacs\.spacemacs.d %DOTFILES%\EDITORS\EMACS\.spacemacs.d
+mklink /D %USERPROFILE%\.config\emacs\doom-emacs\.doom.d %DOTFILES%\EDITORS\EMACS\.doom.d
 REM set DOOMDIR env variable in current shell for `doom sync` and other doom commands
-set DOOMDIR=%USERPROFILE%\emacs\doom-emacs\.doom.d
+set DOOMDIR=%USERPROFILE%\.config\emacs\doom-emacs\.doom.d
 REM set DOOMDIR env variable permanently for `doom sync` and other doom commands
-setx DOOMDIR %USERPROFILE%\emacs\doom-emacs\.doom.d
+setx DOOMDIR %USERPROFILE%\.config\emacs\doom-emacs\.doom.d
