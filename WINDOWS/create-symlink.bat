ddf25a93463d760a155d8958922c1957f9a22ff5
rakesh-mth <46665723+rakesh-mth@users.noreply.github.com>
2020-12-27 19:49:20 -0500

    create .emacs and .emacs-profile.el symlink in windows

--- WINDOWS/create-symlink.bat
+++ WINDOWS/create-symlink.bat
@@ -1,6 +1,8 @@
 if exist %USERPROFILE%\.gitconfig del %USERPROFILE%\.gitconfig
 if exist %USERPROFILE%\.gitconfig-work del %USERPROFILE%\.gitconfig-work
 if exist %USERPROFILE%\.wslconfig del %USERPROFILE%\.wslconfig
+if exist %USERPROFILE%\.emacs del %USERPROFILE%\.emacs
+if exist %USERPROFILE%\.emacs-profiles.el del %USERPROFILE%\.emacs-profiles.el
 if exist %USERPROFILE%\emacs\spacemacs\.spacemacs.d rmdir %USERPROFILE%\emacs\spacemacs\.spacemacs.d
 if exist %USERPROFILE%\emacs\doom-emacs\.doom.d rmdir %USERPROFILE%\emacs\doom-emacs\.doom.d
 if exist %USERPROFILE%\.vimrc del %USERPROFILE%\.vimrc
@@ -8,6 +10,8 @@ if exist %USERPROFILE%\AppData\Local\nvim\init.vim del %USERPROFILE%\AppData\Loc
 
 if not exist %USERPROFILE%\AppData\Local\nvim mkdir %USERPROFILE%\AppData\Local\nvim
 if not exist %USERPROFILE%\.vim mkdir %USERPROFILE%\.vim
+if not exist %USERPROFILE%\emacs\spacemacs mkdir %USERPROFILE%\emacs\spacemacs
+if not exist %USERPROFILE%\emacs\doom-emacs mkdir %USERPROFILE%\emacs\doom-emacs
 
 SET SCRIPTS=f:\DevTrees\scripts
 SET DOTFILES=f:\DevTrees\rakesh-mth\dotfiles
@@ -18,6 +22,8 @@ mklink %USERPROFILE%\.wslconfig %DOTFILES%\WSL\.wslconfig
 mklink %USERPROFILE%\.vimrc %DOTFILES%\EDITORS\VIM\init.vim
 mklink %USERPROFILE%\AppData\Local\nvim\init.vim %DOTFILES%\EDITORS\VIM\init.vim
 REM chemacs - spacemacs and doom-emacs
+mklink %USERPROFILE%\.emacs %DOTFILES%\EDITORS\EMACS\.emacs
+mklink %USERPROFILE%\.emacs-profiles.el %DOTFILES%\EDITORS\EMACS\.emacs-profiles.el
 mklink /D %USERPROFILE%\emacs\spacemacs\.spacemacs.d %DOTFILES%\EDITORS\EMACS\.spacemacs.d
 mklink /D %USERPROFILE%\emacs\doom-emacs\.doom.d %DOTFILES%\EDITORS\EMACS\.doom.d
 REM set DOOMDIR env variable in current shell for `doom sync` and other doom commands
