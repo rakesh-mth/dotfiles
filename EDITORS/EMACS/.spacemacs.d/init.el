d714e4730a8da90a3673552f97889dca4777a84d
rakesh-mth <46665723+rakesh-mth@users.noreply.github.com>
2021-04-02 20:46:58 -0400

    use MesloLGS;2ufont on all os in emacs

--- EDITORS/EMACS/.spacemacs.d/init.el
+++ EDITORS/EMACS/.spacemacs.d/init.el
@@ -534,12 +534,12 @@ If you are unsure, try setting them in `dotspacemacs/user-config' first."
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;; font size sepcific for OS
   ;; (if (eq system-type 'gnu/linux)
-  (if (eq system-type 'darwin)
-    (setq-default dotspacemacs-default-font '("Source Code Pro"
-                                              :size 24.0
-                                              :weight normal
-                                              :width normal
-                                              :powerline-scale 1.1)))
+  ;; (if (eq system-type 'darwin)
+  (setq-default dotspacemacs-default-font '("MesloLGS NF"
+                                            :size 24.0
+                                            :weight normal
+                                            :width normal
+                                            :powerline-scale 1.1))
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;; lsp-clangd backend (already default), set clangd path. For c-c++ layer.
   ;; not needed now since spacemacs can able to use clangd from xcode
