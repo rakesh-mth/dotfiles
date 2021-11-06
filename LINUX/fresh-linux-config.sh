f45ada998152480c296739b3fd940bd20441ff4d
rakesh-mth <46665723+rakesh-mth@users.noreply.github.com>
2021-11-05 22:43:24 -0400

    update freah linux config

--- LINUX/fresh-linux-config.sh
+++ LINUX/fresh-linux-config.sh
@@ -13,10 +13,28 @@ sudo apt-get install curl git uswsusp
 sudo apt-get install build-essential procps curl file git
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # homebrew core
-brew install curl git htop tmux vim neovim emacs fd bat ripgrep the_silver_searcher fzf llvm rust golang nodejs cmake aspell iperf3 gnupg
+# aspell is used with spacemacs
+# xclip is used with neovim for copy paste from clipboard
+# virtualenv is installed for python virtual envs
+brew install curl git htop tmux vim neovim emacs fd bat ripgrep the_silver_searcher fzf llvm rust golang nodejs cmake aspell iperf3 gnupg virtualenv xclip
 # homebrew cask (is not supported on linux). 
 # Error: Installing casks is supported only on macOS
 # brew install homebrew/cask/emacs
+
+
+# python (path must match with config.vim in vim-user-config repo)
+virtualenv -p /home/linuxbrew/.linuxbrew/bin/python3.9 $HOME/.virtualenvs/python3.9
+source /home/rakesh/.virtualenvs/python3.9/bin/activate
+# install all python modules for vim, neovim and emacs developments
+pip install pynvim neovim-remote flake8 isort yapf python-language-server pyls-isort pyls-mypy importmagic epc ptvsd autoflake
+
+
+# EMACS
+# clone spacemacs (path must match from create-symlink.sh)
+git clone https://github.com/syl20bnr/spacemacs ~/.config/emacs/spacemacs/.emacs.d
+# clone doom-emacs (path must match from create-symlink.sh)
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.config/emacs/doom-emacs/.emacs.d
+
 
 # key gen from github (only once, not on all machine)
 # ssh-keygen -t ed25519 -C "rakesh.mth@gmail.com"
