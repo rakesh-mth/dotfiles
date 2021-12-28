673da8425dbd5923f60c66879f2b2fe5ff6dd43d
rakesh-mth <46665723+rakesh-mth@users.noreply.github.com>
2021-12-28 13:40:00 -0500

    use cheovim with LunarVim on windows

--- EDITORS/VIM/profiles.lua
+++ EDITORS/VIM/profiles.lua
@@ -13,6 +13,47 @@
 	A config switcher written in Lua by NTBBloodbath and Vhyrro.
 --]]
 
+-- rakesh - helper setup function
+
+_G.cheovim_profile_setup = function(config_name)
+    -- helper function to join paths
+    local path_sep = vim.loop.os_uname().version:match "Windows" and "\\" or "/"
+    function _G.join_paths(...)
+      local result = table.concat({ ... }, path_sep)
+      return result
+    end
+    -- no need to delete packer_compiled.lua, since config path will be from current cheovim config.
+    -- vim.fn.delete(vim.fn.stdpath("config") .. "/plugin/packer_compiled.lua")
+    -- make sure doom-nvim path for config is in runtimepath
+    vim.opt.rtp:append(join_paths(vim.fn.expand("~"), ".config", "nvim-config", config_name))
+    -- remove original data/site and data/site/after
+    vim.opt.rtp:remove(join_paths(vim.fn.stdpath "data", "site"))
+    vim.opt.rtp:remove(join_paths(vim.fn.stdpath "data", "site", "after"))
+    -- these will not work since cheovim will override default config path
+    -- vim.opt.rtp:remove(vim.fn.stdpath "config")
+    -- vim.opt.rtp:remove(join_paths(vim.fn.stdpath "config", "after"))
+
+    -- Clone the current stdpath function definition into an unused func
+    vim.fn._stdpath_setup = vim.fn.stdpath
+
+    -- Override vim.fn.stdpath to manipulate the data returned by it. Yes, I know, changing core functions
+    -- is really bad practice in any codebase, however this is our only way to make things like doom-nvim etc. work
+    vim.fn.stdpath = function(what)
+      if what:lower() == "data" then
+          local data_path = join_paths(vim.fn._stdpath_setup(what), config_name)
+          vim.opt.rtp:prepend(join_paths(data_path, "site"))
+          vim.opt.rtp:append(join_paths(data_path, "site", "after"))
+          vim.cmd [[let &packpath = &runtimepath]]
+          -- vim.cmd(("echom \"%s\""):format(data_path))
+          return data_path
+      elseif what:lower() == "cache" then
+          local cache_path = join_paths(vim.fn._stdpath_setup(what), config_name, ".cache")
+          return cache_path
+      end
+      return vim.fn._stdpath_setup(what)
+    end
+end
+
 -- Defines the profiles you want to use
 local profiles = {
 	--[[
@@ -27,51 +68,22 @@ local profiles = {
 	More in-depth information can be found in cheovim's README on GitHub.
 	--]]
     rakesh_mth = { "~/workspaces/rakesh-mth/dotfiles/EDITORS/VIM/", {
-        plugins = "packer",
-        preconfigure = "nil",
-      }
+          plugins = "packer",
+          preconfigure = "nil",
+        }
     },
     lunar_nvim = { "~/.config/nvim-config/LunarVim", {
             plugins = "packer",
+            setup = function()
+              cheovim_profile_setup("LunarVim")
+            end,
             preconfigure = "lunarvim"
         }
     },
     doom_nvim = { "~/.config/nvim-config/doom-nvim", {
             plugins = "packer",
             setup = function()
-                -- helper function to join paths
-                local path_sep = vim.loop.os_uname().version:match "Windows" and "\\" or "/"
-                function _G.join_paths(...)
-                  local result = table.concat({ ... }, path_sep)
-                  return result
-                end
-                -- no need to delete packer_compiled.lua, since config path will be from current cheovim config.
-                -- vim.fn.delete(vim.fn.stdpath("config") .. "/plugin/packer_compiled.lua")
-                -- make sure doom-nvim path for config is in runtimepath
-                vim.opt.rtp:append(join_paths(vim.fn.expand("~"), ".config", "nvim-config", "doom-nvim"))
-                -- remove original data/site and data/site/after
-                vim.opt.rtp:remove(join_paths(vim.fn.stdpath "data", "site"))
-                vim.opt.rtp:remove(join_paths(vim.fn.stdpath "data", "site", "after"))
-                -- these will not work since cheovim will override default config path
-                -- vim.opt.rtp:remove(vim.fn.stdpath "config")
-                -- vim.opt.rtp:remove(join_paths(vim.fn.stdpath "config", "after"))
-
-                -- Clone the current stdpath function definition into an unused func
-                vim.fn._stdpath_setup = vim.fn.stdpath
-
-                -- Override vim.fn.stdpath to manipulate the data returned by it. Yes, I know, changing core functions
-                -- is really bad practice in any codebase, however this is our only way to make things like doom-nvim etc. work
-                vim.fn.stdpath = function(what)
-                    if what:lower() == "data" then
-                        local data_path = join_paths(vim.fn._stdpath_setup(what), "doom-nvim")
-                        vim.opt.rtp:prepend(join_paths(data_path, "site"))
-                        vim.opt.rtp:append(join_paths(data_path, "site", "after"))
-                        vim.cmd [[let &packpath = &runtimepath]]
-                        -- vim.cmd(("echom \"%s\""):format(data_path))
-                        return data_path
-                    end
-                    return vim.fn._stdpath_setup(what)
-                end
+              cheovim_profile_setup("doom-nvim")
             end,
             preconfigure = "doom-nvim"
         }
--- WINDOWS/BATCH/lvim.bat
+++ WINDOWS/BATCH/lvim.bat
@@ -1,10 +1,13 @@
-REM using cheovim - choose lunar_nvim config. Not used because some folder (after) has conflict.
-REM start "" nvim-qt.exe --no-ext-tabline -- --cmd "lua load_profile='lunar_nvim'"
+REM using cheovim - choose lunar_nvim config.
+start "" nvim-qt.exe --no-ext-tabline -- --cmd "lua load_profile='lunar_nvim'"
+
+
+REM using environment variable and -u option with init.lua in LunarVim
+:: set LUNARVIM_RUNTIME_DIR=%USERPROFILE%\AppData\Local\nvim-data\LunarVim
+:: set LUNARVIM_CACHE_DIR=%USERPROFILE%\AppData\Local\nvim-data\LunarVim\.cache
+:: set LUNARVIM_CONFIG_DIR=%USERPROFILE%\.config\nvim-config\LunarVim
+:: start "" nvim-qt.exe --no-ext-tabline -- -u %USERPROFILE%\.config\nvim-config\LunarVim\init.lua
+
 
 REM This can be used but needs changes in the lvim.ps1 script or some more symlinks.
-REM Powershell.exe -executionpolicy remotesigned -File  %USERPROFILE%\.config\nvim-config\LunarVim\utils\bin\lvim.ps1
-
-set LUNARVIM_RUNTIME_DIR=%USERPROFILE%\AppData\Local\nvim-data\LunarVim
-set LUNARVIM_CACHE_DIR=%USERPROFILE%\AppData\Local\nvim-data\LunarVim\.cache
-set LUNARVIM_CONFIG_DIR=%USERPROFILE%\.config\nvim-config\LunarVim
-start "" nvim-qt.exe --no-ext-tabline -- -u %USERPROFILE%\.config\nvim-config\LunarVim\init.lua
+:: Powershell.exe -executionpolicy remotesigned -File  %USERPROFILE%\.config\nvim-config\LunarVim\utils\bin\lvim.ps1
