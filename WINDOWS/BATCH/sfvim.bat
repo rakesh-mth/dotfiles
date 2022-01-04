6d5d3b3e6e0dc467b770e4bcb34a98017db58433
rakesh-mth <46665723+rakesh-mth@users.noreply.github.com>
2022-01-04 10:12:56 -0500

    add sfvim.bat file

---
+++ WINDOWS/BATCH/sfvim.bat
@@ -0,0 +1,5 @@
+REM using cheovim - choose rakesh_mth config.
+start "" FVim.exe --cmd "lua load_profile='rakesh_mth'"
+
+REM use init.vim file directly
+:: start "" FVim.exe -u f:\DevTrees\rakesh-mth\dotfiles\EDITORS\VIM\init.vim
