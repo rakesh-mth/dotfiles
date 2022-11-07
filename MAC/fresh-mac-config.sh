c54f9d87038a8c7668b282ba0db1893929d20e96
rakesh-mth <46665723+rakesh-mth@users.noreply.github.com>
2022-11-07 10:10:27 -0500

    configure fzf after install

--- MAC/fresh-mac-config.sh
+++ MAC/fresh-mac-config.sh
@@ -25,6 +25,10 @@ brew install neovim vim ctags wget xz jfrog-cli-go python@2 python@3 groovy node
 ################################################################################
 
 ################################################################################
+###### install fzf shell completion and key binding
+$(brew --prefix)/opt/fzf/install
+
+################################################################################
 ###### FONT FOR HUNSPELL
 pushd $HOME/Library/Spelling
 wget -O en_US.aff  https://cgit.freedesktop.org/libreoffice/dictionaries/plain/en/en_US.aff?id=a4473e06b56bfe35187e302754f6baaa8d75e54f
